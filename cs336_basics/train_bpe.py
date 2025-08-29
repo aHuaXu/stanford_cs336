f922fcf4066c8f4fc5055507e586ca3423b163cf
jiahua <1530784218@qq.com>
2025-08-29 22:53:33 +0800

    train_bpe(assignment1#L9)

---
+++ cs336_basics/train_bpe.py
@@ -0,0 +1,164 @@
+import os
+import regex as re
+from multiprocessing import Pool
+from typing import Generator, Tuple, List, Pattern, Union
+from cs336_basics.pretokenization_example import find_chunk_boundaries
+
+PAT = re.compile(
+    r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+)
+
+def init_vocab(special_tokens: list[str]) -> dict[int, bytes]:
+    vocab = {i: bytes([i]) for i in range(256)}
+
+    for token in special_tokens:
+        bytes_token = token.encode("utf-8")
+        vocab[len(vocab)] = bytes_token
+    return vocab
+
+
+def word2bytes(word: str) -> tuple[bytes, ...]:
+    a = list(word.encode("utf-8"))
+    return tuple(bytes([b]) for b in a)
+
+
+def chunk_generator(
+    input_path: str | os.PathLike,
+    num_processes: int,
+    special_tokens: List[str],
+) -> Generator[tuple[str, List[str]], None, None]:
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            chunk = chunk.replace("\r\n", "\n")  # 兼容windows
+            yield chunk, special_tokens
+
+
+def process_chunk(
+    chunk: str,
+    special_tokens: List[str],
+) -> dict[tuple[bytes, ...], int]:
+    separator = "|".join([re.escape(token) for token in special_tokens])
+    separator_pat = re.compile(separator)
+
+    pre_token_counts: dict[tuple[bytes, ...], int] = {}
+    # remove special tokens
+    sub_chunks = separator_pat.split(chunk)
+    # Run pre-tokenization on your chunk and store the counts for each pre-token
+    for sub_chunk in sub_chunks:
+        for match in PAT.finditer(sub_chunk):
+            token = match.group()
+            token_tuple = word2bytes(token)
+            if len(token_tuple) < 2:
+                continue
+            pre_token_counts[token_tuple] = pre_token_counts.get(token_tuple, 0) + 1
+    return pre_token_counts
+
+
+def train_bpe(
+    input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str],
+    **kwargs,
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    """Given the path to an input corpus, run train a BPE tokenizer and
+    output its vocabulary and merges.
+
+    Args:
+        input_path (str | os.PathLike): Path to BPE tokenizer training data.
+        vocab_size (int): Total number of items in the tokenizer's vocabulary (including special tokens).
+        special_tokens (list[str]): A list of string special tokens to be added to the tokenizer vocabulary.
+            These strings will never be split into multiple tokens, and will always be
+            kept as a single token. If these special tokens occur in the `input_path`,
+            they are treated as any other string.
+
+    Returns:
+        tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+            vocab:
+                The trained tokenizer vocabulary, a mapping from int (token ID in the vocabulary)
+                to bytes (token bytes)
+            merges:
+                BPE merges. Each list item is a tuple of bytes (<token1>, <token2>),
+                representing that <token1> was merged with <token2>.
+                Merges are ordered by order of creation.
+    """
+    # 1. Vocab
+    # dict[int, bytes]
+    vocab: dict[int, bytes] = init_vocab(special_tokens)
+
+    # dict[tuple[bytes], int]
+    pre_token_counts: dict[tuple[bytes, ...], int] = {}
+
+    separator = "|".join([re.escape(token) for token in special_tokens])
+    separator_pat = re.compile(separator)
+
+    # 2. Pre-tokenization
+    num_processes = 2   # test_train_bpe_speed will fail in windows if it's set with 4 ...
+    with Pool(processes=num_processes) as pool:
+        results = pool.starmap(
+            process_chunk,
+            chunk_generator(input_path, num_processes, special_tokens)
+        )
+        for one_chunk_count in results:
+            for token, count in one_chunk_count.items():
+                pre_token_counts[token] = pre_token_counts.get(token, 0) + count
+    # print("pre_token_counts:{}".format(pre_token_counts))
+
+    # 3. count the initial token counts
+    token_pairs_counts: dict[tuple[bytes, bytes], int] = {}
+    merges: list[tuple[bytes, bytes]] = []
+    for pre_token, count in pre_token_counts.items():
+        for pair in zip(pre_token[:-1], pre_token[1:]):
+            # (bytes, bytes)
+            token_pairs_counts[pair] = token_pairs_counts.get(pair, 0) + count
+
+    # 4. Merges
+    while len(vocab) < vocab_size:
+        # get the most frequent pair, if there are multiple pairs with the same frequency, choose the lexicographically greater pair
+        most_frequent_pair, _ = max(
+            token_pairs_counts.items(), key=lambda x: (x[1], x[0])
+        )
+        # print("most_frequent_pair:{}, token_pairs_counts:{}".format(most_frequent_pair, token_pairs_counts))
+
+        merges.append(most_frequent_pair)
+        merged_token = most_frequent_pair[0] + most_frequent_pair[1]
+        vocab[len(vocab)] = merged_token
+
+        # replace the pre_token with the merged token
+        for pre_token, count in list(pre_token_counts.items()):
+            n = len(pre_token)
+            if most_frequent_pair not in zip(pre_token[:-1], pre_token[1:]):
+                continue
+
+            # generate the new pre_token
+            new_pre_token: list[bytes] = []
+            i = 0
+            while i < n:
+                # match the most frequent pair
+                if (
+                    i < n - 1
+                    and pre_token[i] == most_frequent_pair[0]
+                    and pre_token[i + 1] == most_frequent_pair[1]
+                ):
+                    new_pre_token.append(merged_token)
+                    i += 2
+                else:
+                    new_pre_token.append(pre_token[i])
+                    i += 1
+
+            # clear pre_token pair count
+            for pair in zip(pre_token[:-1], pre_token[1:]):
+                token_pairs_counts[pair] -= count
+                if token_pairs_counts[pair] == 0:
+                    del token_pairs_counts[pair]
+            # count the new_pre_token pair count
+            for pair in zip(new_pre_token[:-1], new_pre_token[1:]):
+                token_pairs_counts[pair] = token_pairs_counts.get(pair, 0) + count
+
+            # update the pre_token_counts with the new_pre_token
+            pre_token_counts[tuple(new_pre_token)] = count
+            del pre_token_counts[pre_token]
+
+    return vocab, merges
